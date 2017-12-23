bd71a10ad423a0707208bb45c8c194ee98dcfa57
Marco Volkert <marco.volkert24@gmx.de>
2017-12-23 19:28:11 +0100

    refactored

--- download.py
+++ download.py
@@ -1,55 +1,58 @@
-#http://docs.python-guide.org/en/latest/scenarios/scrape/
-#http://stackabuse.com/download-files-with-python/
+#!/usr/bin/env python3
+
+"""
+collection of download operations
+http://docs.python-guide.org/en/latest/scenarios/scrape/
+http://stackabuse.com/download-files-with-python/
+"""
+
+__author__ = "Marco Volkert"
+__copyright__ = "Copyright 2017, Marco Volkert"
+__email__ = "marco.volkert24@gmx.de"
+__status__ = "Development"
+
 
 from lxml import html
 import requests
 import os
-    
-def getHrefs(mainpage, subpage,css='',contains=''):
-    page = requests.get(mainpage+subpage)
+
+
+def getHrefs(mainpage, subpage, css='', contains=''):
+    page = requests.get(mainpage + subpage)
     tree = html.fromstring(page.content)
-    aList = tree.xpath('//a'+css)
-    hrefs = [ x.get("href") for x in aList]
-    if not contains=='': hrefs = [x for x in hrefs if contains in x]
-    #print(hrefs)
-    return hrefs       
-    
-def downloadPics(mainpage,name):
-    maindest = 'F:\\Bilder\\bearbeitung\\webpic\\'
-    mainname = mainpage.replace('http://','').replace('.com','')
-    namedest=maindest+mainname+'\\'+name
-    if not os.path.isdir(namedest): os.makedirs(namedest)
-    
-    galleries = getHrefs(mainpage,'/model/'+name+'/',contains='gallery')
-    for i,gallery in enumerate(galleries):
-        pics=getHrefs(mainpage,gallery,css='[@class="fancybox"]')
-        dest=namedest+'\\'+str(i)
+    aList = tree.xpath('//a' + css)
+    hrefs = [x.get("href") for x in aList]
+    hrefs = [x for x in hrefs if contains in x]
+    # print(hrefs)
+    return hrefs
+
+
+def downloadPics(mainpage, name):
+    maindest = 'F:\\Video\\z_Bearbeitung\\new\\photoseries\\'
+    mainname = mainpage.replace('http://', '').replace('.com', '')
+    namedest = maindest + mainname + '\\' + name
+    os.makedirs(namedest, exist_ok=True)
+
+    galleries = getHrefs(mainpage, '/model/' + name + '/', contains='gallery')
+    for i, gallery in enumerate(galleries):
+        pics = getHrefs(mainpage, gallery, css='[@class="fancybox"]')
+        dest = namedest + '\\' + str(i)
         print(dest)
-        if not os.path.isdir(dest): os.makedirs(dest)
+        os.makedirs(dest, exist_ok=True)
         for pic in pics:
-            downloadFile(pic,dest)
-        
-def downloadFile(picUrl, dest):        
+            downloadFile(pic, dest)
+
+
+def downloadFile(picUrl, dest):
     page = requests.get(picUrl)
-    filename=dest+'\\'+picUrl.split('/')[-1]
-    #print(filename)
-    with open(filename, 'wb') as f:  
+    filename = dest + '\\' + picUrl.split('/')[-1]
+    # print(filename)
+    with open(filename, 'wb') as f:
         f.write(page.content)
-        
+
+
 def downloadMulti():
-    #names=['katy-rios','kiara-lord','sara-kay','vinna-reed','alice-march']
-    #mainpage='http://alsscangirlz.com'
-    #for name in names:
-    #    downloadPics(mainpage,name)
-    #names=['milla','jessica','foxy-di','silvie','angelica']
-    #mainpage='http://matrixteens.com'
-    #for name in names:
-    #    downloadPics(mainpage,name)
-    #names=['luba','selma','foxy-di','larisa-a','irishka','nikola','vera']
-    #mainpage='http://xmodelpics.com'
-    #for name in names:
-    #    downloadPics(mainpage,name)    
-    names=['daniel-sea']
-    mainpage='http://www.ametart.com'
+    names = ['daniel-sea']
+    mainpage = 'http://www.ametart.com'
     for name in names:
-        downloadPics(mainpage,name)       
+        downloadPics(mainpage, name)
--- rename.py
+++ rename.py
@@ -9,6 +9,7 @@ __copyright__ = "Copyright 2017, Marco Volkert"
 __email__ = "marco.volkert24@gmx.de"
 __status__ = "Development"
 
+import os
 # for reloading
 from IPython import get_ipython
 
@@ -30,7 +31,7 @@ def renameFolderCount(name="Name", start=1):
         dirCounter += 1
 
 
-renameFolderCount.path = "F:\\Video\\z_Bearbeitung\\Neue Bilder und Videos\\Fotoserien\\"
+renameFolderCount.path = "F:\\Video\\z_Bearbeitung\\new\\photoseries\\"
 
 
 def renameFolderCountMulti():
@@ -41,24 +42,23 @@ def renameFolderCountMulti():
             renameFolderCount(dirname, 1)
 
 
-def renameIndexNorm(subpath="", write=False):
+def normalizeCountersKeepName(subpath="", write=False):
     import re
     if not subpath == "": subpath = "\\" + subpath
-    inpath = renameIndexNorm.path + subpath
+    inpath = normalizeCountersKeepName.path + subpath
     print(inpath)
     dirCounter = 1
     fileCounter = 1
     lastNameMain = ""
     lastNameMid = ""
     matchreg = r"^([-\w]+)_([0-9]+)_([0-9]+)"
-    if write: temppostfix = renameTemp2(inpath)
+    if write: _renameTemp(inpath)
     for (dirpath, dirnames, filenames) in os.walk(inpath):
         for filename in filenames:
             match = re.search(matchreg, filename)
             if not match:
                 print("no match", dirpath, filename)
-                newFilename = filename[:filename.rfind(".")] + ".jpg"
-                if write: renameInPlace(dirpath, filename, newFilename)
+                if write: _renameTempBack(dirpath, filename)
                 continue
             nameMain = match.group(1)
             nameMid = match.group(2)
@@ -72,32 +72,31 @@ def renameIndexNorm(subpath="", write=False):
                 fileCounter += 1
             newFilename = nameMain + "_%02d_%02d" % (dirCounter, fileCounter) + ".jpg"
             print(newFilename)
-            if write: renameInPlace(dirpath, filename, newFilename)
+            if write: _renameInPlace(dirpath, filename, newFilename)
             lastNameMain = nameMain
             lastNameMid = nameMid
 
 
-renameIndexNorm.path = "F:\\Video\\z_Bearbeitung\\Bilder\\sortiert\\best\\Mari"
+normalizeCountersKeepName.path = "F:\\Video\\z_Bearbeitung\\Bilder\\sorted\\best"
 
 
-def renameIndexNorm2(subpath="", name="", start=1, write=False):
+def normalizeCounters(subpath="", name="", start=1, write=False):
     import re
     if not subpath == "": subpath = "\\" + subpath
-    inpath = renameIndexNorm2.path + subpath
+    inpath = normalizeCounters.path + subpath
     if not name == "":  name += "_"
     dirCounter = start - 1
     fileCounter = 1
     lastNameMid = ""
     matchreg = r"([0-9]+)_([0-9]+)."
-    if write: temppostfix = renameTemp2(inpath)
+    if write: _renameTemp(inpath)
     for (dirpath, dirnames, filenames) in os.walk(inpath):
         print("renameIndexNorm2:", dirpath)
         for filename in filenames:
             match = re.search(matchreg, filename)
             if not match:
                 print("no match", dirpath, filename)
-                newFilename = filename[:filename.rfind(".")] + ".jpg"
-                if write: renameInPlace(dirpath, filename, newFilename)
+                if write: _renameTempBack(dirpath, filename)
                 continue
             nameMid = match.group(1)
             if not nameMid == lastNameMid:
@@ -107,53 +106,70 @@ def renameIndexNorm2(subpath="", name="", start=1, write=False):
                 fileCounter += 1
             newFilename = name + "%02d_%02d" % (dirCounter, fileCounter) + ".jpg"
             print(dirpath, newFilename)
-            if write: renameInPlace(dirpath, filename, newFilename)
+            if write: _renameInPlace(dirpath, filename, newFilename)
             lastNameMid = nameMid
     return dirCounter
 
 
-renameIndexNorm2.path = "F:\\Video\\z_Bearbeitung\\Bilder\\websites"
+normalizeCounters.path = "F:\\Video\\z_Bearbeitung\\Bilder\\websites"
 
 
-def renameIndexNorm2Multi(subpath=""):
+def normalizeCountersMultiDirname(subpath=""):
     if not subpath == "": subpath = "\\" + subpath
-    inpath = renameIndexNorm2.path + subpath
-    dirCounter = 1
+    inpath = normalizeCounters.path + subpath
     for (dirpath, dirnames, filenames) in os.walk(inpath):
         if not inpath == dirpath: continue
         for dirname in dirnames:
             print("renameIndexNorm2Multi:", dirname)
-            renameIndexNorm2(dirname, dirname, dirCounter)
+            normalizeCounters(dirname, dirname, 1)
 
 
-def renameIndexNorm2Multi2(subpath="", name=""):
+def normalizeCountersMulti(subpath="", name=""):
     if not subpath == "": subpath = "\\" + subpath
-    inpath = renameIndexNorm2.path + subpath
+    inpath = normalizeCounters.path + subpath
     dirCounter = 0
     for (dirpath, dirnames, filenames) in os.walk(inpath):
         if not inpath == dirpath: continue
         for dirname in dirnames:
-            dirCounter = renameIndexNorm2(subpath + "\\" + dirname, name, dirCounter + 1)
-            
-            
-def renameNameFolders(subpath="", write=True):
-    #from name-sirname to Name Sirname
+            dirCounter = normalizeCounters(dirname, name, dirCounter + 1)
+
+
+def FoldersToUpper(subpath="", write=True):
+    # from name-sirname to Name Sirname
     if not subpath == "": subpath = "\\" + subpath
-    inpath = renameNameFolders.path + subpath
+    inpath = FoldersToUpper.path + subpath
     for (dirpath, dirnames, filenames) in os.walk(inpath):
         if not inpath == dirpath: continue
         for dirname in dirnames:
-            parts=dirname.split('-')
-            newName=''
+            parts = dirname.split('-')
+            newName = ''
             for part in parts:
-                newName+=part[0].upper()
-                newName+=part[1:]
-                newName+=' '
-            newName=newName[:-1]    
+                newName += part[0].upper()
+                newName += part[1:]
+                newName += ' '
+            newName = newName[:-1]
             print(newName)
-            if write: renameInPlace(dirpath, dirname, newName)
-renameNameFolders.path = "F:\\Video\\z_Bearbeitung\\Neue Bilder und Videos\\Fotoserien"        
+            if write: _renameInPlace(dirpath, dirname, newName)
+
+
+FoldersToUpper.path = "F:\\Video\\z_Bearbeitung\\new\\photoseries"
+
 
 def _renameInPlace(dirpath, oldFilename, newFilename):
     os.rename(dirpath + "\\" + oldFilename, dirpath + "\\" + newFilename)
-        
+
+
+def _renameTemp(inpath):
+    temppostfix = "temp"
+    if not os.path.isdir(inpath):
+        print('not found directory: ' + inpath)
+        return
+    for (dirpath, dirnames, filenames) in os.walk(inpath):
+        for filename in filenames:
+            os.rename(dirpath + "\\" + filename, dirpath + "\\" + filename + temppostfix)
+    return temppostfix
+
+
+def _renameTempBack(dirpath, filename):
+    newFilename = filename[:filename.rfind(".")] + ".jpg"
+    _renameInPlace(dirpath, filename, newFilename)
