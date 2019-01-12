5211c775c2d619875a3405df4ee9d0221fa276c3
Marco Volkert <marco.volkert24@gmx.de>
2019-01-12 15:21:13 +0100

    refactoring

--- downloading.py
+++ downloading.py
@@ -16,81 +16,96 @@ import requests
 import os
 
 
-def getHrefs(mainpage, subpage, css='', contains=''):
-    print(mainpage + subpage)
-    page = requests.get(mainpage + subpage)
-    tree = html.fromstring(page.content)
+def getHrefs(page, css='', contains=''):
+    tree = html.fromstring(get_page_content(page))
     aList = tree.xpath('//a' + css)
     hrefs = [x.get("href") for x in aList]
     hrefs = [href for href in hrefs if href and contains in href]
-    hrefs = [href if mainpage in href else mainpage + href for href in hrefs]
     return hrefs
 
 
-def downloadPics(mainpage, name, subSide="", g_css='', g_contains='', p_css='', p_contains="",
-                 use_gallery_name=True):
+def downloadFiles(mainpage, name, subSide="", g_css='', g_contains='', f_css='', f_contains="",
+                  g_part=None, f_part=-1, ext=""):
     maindest = os.getcwd()
-    replacements = ['http://', 'www.', '.com', '.de']
-    mainname = mainpage
-    for replacement in replacements:
-        mainname = mainname.replace(replacement, '')
-    namedest = os.path.join(maindest, mainname, name)
+    mainname = _strip_url(mainpage)
+    namedest = os.path.join(maindest, mainname, name.replace('/', '-'))
     os.makedirs(namedest, exist_ok=True)
 
     ofile = open(os.path.join(maindest, "download.txt"), 'a')
-    http_path = '/' + subSide + '/' + name
-    if not name.endswith("html"):
-        http_path += "/"
-    galleries = getHrefs(mainpage, http_path, g_css, g_contains)
+    http_path = _build_http_path(name, subSide)
+    galleries = getHrefs(mainpage + http_path, g_css, g_contains)
     for i, gallery in enumerate(galleries):
-        if use_gallery_name:
-            gallery_name = gallery.split("/")[-1]
+        if g_part:
+            gallery_name = gallery.split("/")[g_part]
         else:
             gallery_name = '%03d' % i
         dest = os.path.join(namedest, gallery_name)
         print(dest)
         os.makedirs(dest, exist_ok=True)
-        pics = getHrefs(gallery, "", p_css, p_contains)
-        ofile.write(" ".join([mainname, name, gallery_name, pics[0].split("/")[-1], gallery]) + "\n")
-        for pic in pics:
-            if use_gallery_name:
-                downloadPdf(pic, dest)
-            else:
-                downloadFile(pic, dest)
+        downloadFile(mainpage + gallery, dest)
+        fileUrls = getHrefs(mainpage + gallery, f_css, f_contains)
+        ofile.write(" ".join([mainname, name, gallery_name, fileUrls[0].split("/")[f_part], gallery]) + "\n")
+        for fileUrl in fileUrls:
+            if mainpage not in fileUrl:
+                fileUrl = mainpage + fileUrl
+            downloadFile(fileUrl, dest, f_part, ext)
     ofile.close()
 
 
-def downloadPicsMulti(mainpage, names, subSide="", g_css='', g_contains='', p_css='', p_contains=""):
+def downloadFilesMulti(mainpage, names, subSide="", g_css='', g_contains='', f_css='', f_contains=""):
     for name in names:
-        downloadPics(mainpage, name, subSide, g_css, g_contains, p_css, p_contains)
+        downloadFiles(mainpage, name, subSide, g_css, g_contains, f_css, f_contains)
 
 
-def downloadPicsFromGallery(mainpage, subpage, css='', contains=""):
+def downloadFilesFromGallery(mainpage, subpage, css='', contains=""):
     maindest = os.getcwd()
-    mainname = mainpage.replace('http://', '').replace('.com', '')
+    mainname = _strip_url(mainpage)
     dest = os.path.join(maindest, mainname)
     os.makedirs(dest, exist_ok=True)
 
-    pics = getHrefs(mainpage, subpage, css, contains)
-    print(dest)
+    pics = getHrefs(mainpage + subpage, css, contains)
     os.makedirs(dest, exist_ok=True)
     for pic in pics:
         downloadFile(pic, dest)
 
 
-def downloadFile(picUrl, dest):
-    page = requests.get(picUrl)
-    filename = os.path.join(dest, picUrl.split('/')[-1])
+def downloadFile(url, dest, part=-1, ext=""):
+    filename = os.path.join(dest, _url_to_filename(url, part, ext))
     with open(filename, 'wb') as f:
-        f.write(page.content)
+        f.write(get_page_content(url))
 
 
-def downloadPdf(picUrl, dest):
-    cookies = {"JSESSIONID": "3E7441071E5F19B7BC352A2A951F06B2.springboard3",
-               "spr_prophy_cookie": "true",
-               "springboard_user": "NG8zbzR2NDIxY3ZkanIxaG0x"}
-    print(picUrl)
-    page = requests.get(picUrl, cookies=cookies)
-    filename = os.path.join(dest, picUrl.split('/')[-2] + ".pdf")
-    with open(filename, 'wb') as f:
-        f.write(page.content)
+def get_page_content(url):
+    print("get: " + url)
+    page = requests.get(url, cookies=get_page_content.cookies)
+    return page.content
+
+
+get_page_content.cookies = {"JSESSIONID": "3E7441071E5F19B7BC352A2A951F06B2.springboard3",
+                            "spr_prophy_cookie": "true",
+                            "springboard_user": "NG8zbzR2NDIxY3ZkanIxaG0x"}
+
+
+def _strip_url(url):
+    replacements = ['http://', 'www.', '.com', '.de']
+    name = url
+    for replacement in replacements:
+        name = name.replace(replacement, '')
+    return name
+
+
+def _build_http_path(name, subSide="") -> str:
+    http_path = '/'
+    if subSide:
+        http_path += subSide + '/'
+    http_path += name
+    if not name.endswith("html"):
+        http_path += "/"
+    return http_path
+
+
+def _url_to_filename(url, part=-1, ext="") -> str:
+    filename = url.split('/')[part]
+    if ext:
+        filename = filename.rsplit(".", 1)[0] + ext
+    return filename
