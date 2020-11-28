5bc081bbe9aeb20b9063b6ed57c11a90185e201b
Marco Volkert <marco.volkert24@gmx.de>
2020-11-28 19:30:28 +0100

    replacing for m3u8 files to old ipod destination with mp3 instead of m4a

--- filetools/replacing.py
+++ filetools/replacing.py
@@ -4,15 +4,12 @@ import os
 __all__ = ["replace"]
 
 
-def replace(reverse=False):
+def replace(output: str, source_key="PC"):
     csv_filename = "mapping.csv"
     csv.register_dialect('semicolon', delimiter=';', lineterminator='\r\n')
     with open(csv_filename, "r") as csv_file:
-        reader = csv.reader(csv_file, dialect='semicolon')
+        reader = csv.DictReader(csv_file, dialect='semicolon')
         for row in reader:
-            old, new = row[:2]
-            if reverse:
-                new, old = old, new
             for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
                 for filename in filenames:
                     if filename == csv_filename:
@@ -20,8 +17,24 @@ def replace(reverse=False):
                     outlines = []
                     with open(filename, "r") as file:
                         for line in file:
-                            if not line.startswith('#'):
-                                line = line.replace(old, new)
+                            if not line.startswith('#') and row[source_key] in line:
+                                if output == "IPod":
+                                    if ".m4a" in line:
+                                        line = line.replace(row[source_key], row[output]).replace(".m4a", ".mp3")
+                                else:
+                                    line = line.replace(row[source_key], row[output])
                             outlines.append(line)
                     with open(filename, "w") as file:
                         file.writelines(outlines)
+
+    outlines = []
+    for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
+        for filename in filenames:
+            if filename == csv_filename:
+                continue
+            with open(filename, "r") as file:
+                for line in file:
+                    if line not in outlines:
+                        outlines.append(line)
+    with open("Dance.m3u8", "w") as file:
+        file.writelines(outlines)
