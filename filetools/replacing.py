e1c1dd377fbb03a0421e6be23454072af6123d3c
Marco Volkert <marco.volkert24@gmx.de>
2020-11-29 09:29:59 +0100

    improve structure of replace_playlists

--- filetools/replacing.py
+++ filetools/replacing.py
@@ -1,40 +1,46 @@
 import csv
 import os
 
-__all__ = ["replace"]
+__all__ = ["replace_playlists"]
+
+from typing import List, Union, OrderedDict, Dict
 
 
-def replace(output: str, source_key="PC"):
+def replace_playlists(output: str, source_key="PC"):
+    cwd = os.getcwd()
+    out_dir = os.path.join(cwd, output)
+    os.makedirs(out_dir, exist_ok=True)
+    all_lines = []
     csv_filename = "mapping.csv"
-    csv.register_dialect('semicolon', delimiter=';', lineterminator='\r\n')
-    with open(csv_filename, "r") as csv_file:
-        reader = csv.DictReader(csv_file, dialect='semicolon')
-        for row in reader:
-            for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
-                for filename in filenames:
-                    if filename == csv_filename:
-                        continue
-                    outlines = []
-                    with open(filename, "r") as file:
-                        for line in file:
-                            if not line.startswith('#') and row[source_key] in line:
+    mapping_rows = _read_mapping(csv_filename)
+    for (dirpath, dirnames, filenames) in os.walk(cwd):
+        if not dirpath == cwd:
+            break
+        for filename in filenames:
+            if filename == csv_filename:
+                continue
+            outlines = []
+            with open(filename, "r") as file:
+                for line in file:
+                    if not line.startswith('#'):
+                        for row in mapping_rows:
+                            if row[source_key] in line:
                                 if output == "IPod":
                                     if ".m4a" in line:
                                         line = line.replace(row[source_key], row[output]).replace(".m4a", ".mp3")
                                 else:
                                     line = line.replace(row[source_key], row[output])
-                            outlines.append(line)
-                    with open(filename, "w") as file:
-                        file.writelines(outlines)
+                    outlines.append(line)
+                    all_lines.append(line)
+            with open(os.path.join(out_dir, filename), "w") as file:
+                file.writelines(outlines)
 
-    outlines = []
-    for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
-        for filename in filenames:
-            if filename == csv_filename:
-                continue
-            with open(filename, "r") as file:
-                for line in file:
-                    if line not in outlines:
-                        outlines.append(line)
-    with open("Dance.m3u8", "w") as file:
-        file.writelines(outlines)
+    with open(os.path.join(output, "combined.m3u8"), "w") as file:
+        file.writelines(all_lines)
+
+
+def _read_mapping(csv_filename: str) -> List[Union[Dict[str, str], OrderedDict[str, str]]]:
+    csv.register_dialect('semicolon', delimiter=';', lineterminator='\r\n')
+    with open(csv_filename, "r") as csv_file:
+        reader = csv.DictReader(csv_file, dialect='semicolon')
+        return [row for row in reader]
